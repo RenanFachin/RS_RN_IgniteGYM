f77addda950b48eea0f9753120427570eae2268a
Renan <renan.fachin@hotmail.com>
2023-10-05 15:38:09 -0300

    feat: Cria as rotas de autenticação (stack navigator) e cria tipagem

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,24 @@
+// https://reactnavigation.org/docs/getting-started/
+import { createNativeStackNavigator, NativeStackNavigationProp } from '@react-navigation/native-stack'
+
+import { SignIn } from '@screens/Signin'
+import { SignUp } from '@screens/SignUp'
+
+// https://reactnavigation.org/docs/typescript/
+type AuthRoutes = {
+  signIn: undefined;
+  register: undefined;
+}
+
+export type AuthNavigatorRoutesProps = NativeStackNavigationProp<AuthRoutes>
+
+const { Navigator, Screen } = createNativeStackNavigator<AuthRoutes>()
+
+export function AuthRoutes() {
+  return (
+    <Navigator>
+      <Screen name='signIn' component={SignIn} />
+      <Screen name='register' component={SignUp}/>
+    </Navigator>
+  )
+}
