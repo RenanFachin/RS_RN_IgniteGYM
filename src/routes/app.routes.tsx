b89b49b7eb9f7a6b6c8ee74a2f9353fe658c1bb0
Renan <renan.fachin@hotmail.com>
2023-10-05 17:12:27 -0300

    style: Customiza a bottomTab da aplicação (remove label e adicona icon)

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,5 +1,9 @@
 import { createBottomTabNavigator, BottomTabNavigationProp } from '@react-navigation/bottom-tabs'
+import { useTheme } from 'native-base'
 
+import HomeSVG from '@assets/home.svg'
+import HistorySVG from '@assets/history.svg'
+import ProfileSVG from '@assets/profile.svg'
 
 import { Home } from '@screens/Home'
 import { History } from '@screens/History'
@@ -18,12 +22,61 @@ export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>()
 
 export function AppRoutes() {
+  const theme = useTheme()
+
+  // Definindo o tamanho em uma constante auxiliar
+  const iconSize = theme.sizes[6]
+
   return (
-    <Navigator screenOptions={{headerShown: false}}>
+    <Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
       {/* A ordem importa */}
-      <Screen name='home' component={Home} />
-      <Screen name='history' component={History} />
-      <Screen name='profile' component={Profile} />
+      <Screen
+        name='home'
+        component={Home}
+        options={
+          {
+            tabBarIcon: ({ color }) => (
+              <HomeSVG
+                fill={color}
+                width={iconSize}
+                height={iconSize}
+              />
+            )
+          }
+        }
+      />
+
+      <Screen
+        name='history'
+        component={History}
+        options={
+          {
+            tabBarIcon: ({ color }) => (
+              <HistorySVG
+                fill={color}
+                width={iconSize}
+                height={iconSize}
+              />
+            )
+          }
+        }
+      />
+
+      <Screen
+        name='profile'
+        component={Profile}
+        options={
+          {
+            tabBarIcon: ({ color }) => (
+              <ProfileSVG
+                fill={color}
+                width={iconSize}
+                height={iconSize}
+              />
+            )
+          }
+        }
+      />
 
 
       <Screen name='exercise' component={Exercise} />
