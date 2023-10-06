9a275c552162dc15c1e6fb3814e8fa60f0466e3e
Renan <renan.fachin@hotmail.com>
2023-10-06 00:12:51 -0300

    feat: cria componente de ExerciseCard

---
+++ src/components/ExerciseCard.tsx
@@ -0,0 +1,39 @@
+import { HStack, Heading, Image, Text, VStack, Icon } from 'native-base'
+import { TouchableOpacity, TouchableOpacityProps } from 'react-native'
+import { Entypo } from '@expo/vector-icons'
+
+interface ExerciseCardProps extends TouchableOpacityProps { }
+
+export function ExerciseCard({ ...props }: ExerciseCardProps) {
+  return (
+    <TouchableOpacity {...props}>
+      <HStack bg="gray.500" alignItems="center" p={2} pr={4} rounded="md" mb={3}>
+        <Image
+          source={{ uri: 'https://thumb.mais.uol.com.br/16669847-large.jpg?ver=0' }}
+          alt='Imagem do exercício'
+          w={16}
+          h={16}
+          rounded="md"
+          mr={4}
+          resizeMode="center"
+        />
+
+        <VStack flex={1}>
+          <Heading fontSize="lg" color="white">
+            Serrote
+          </Heading>
+
+          <Text fontSize="sm" color="gray.200" mt={1} numberOfLines={2}>
+            3 séries x 10 repetições
+          </Text>
+        </VStack>
+
+        <Icon 
+          as={Entypo}
+          name='chevron-thin-right'
+          color="gray.300"
+        />
+      </HStack>
+    </TouchableOpacity>
+  )
+}
