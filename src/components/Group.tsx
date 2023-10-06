71114f3afba4c92f00e94ff1c0412f63d6c762ef
Renan <renan.fachin@hotmail.com>
2023-10-05 23:11:43 -0300

    feat: cria estado para definição de group selecionado no momento

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -3,9 +3,10 @@ import { Text, Pressable, IPressableProps } from "native-base";
 
 interface GroupProps extends IPressableProps {
   name: string;
+  isActive: boolean;
 }
 
-export function Group({ name, ...props }: GroupProps) {
+export function Group({ name, isActive, ...props }: GroupProps) {
   return (
     <Pressable
       mr={3}
@@ -16,13 +17,19 @@ export function Group({ name, ...props }: GroupProps) {
       justifyContent="center"
       alignItems="center"
       overflow="hidden"
+      isPressed={isActive}
       _pressed={{
         borderColor: "green.500",
         borderWidth: 1
       }}
       {...props}
     >
-      <Text color="gray.200" textTransform="uppercase" fontSize="xs" fontWeight="bold">
+      <Text
+        color={isActive ? "green.500" : "gray.200"}
+        textTransform="uppercase"
+        fontSize="xs"
+        fontWeight="bold"
+      >
         {name}
       </Text>
     </Pressable>
