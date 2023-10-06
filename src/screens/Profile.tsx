a03bbea6b7aabf09766f52b83f882db2ca9230e8
Renan <renan.fachin@hotmail.com>
2023-10-06 17:49:24 -0300

    feat: Finaliza página de profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
 // https://docs.nativebase.io/skeleton
-import { Center, ScrollView, VStack, Skeleton } from "native-base";
+import { Center, ScrollView, VStack, Skeleton, Text, Heading } from "native-base";
+import { TouchableOpacity } from "react-native";
 
 const PHOTO_SIZE = 33
 
 export function Profile() {
-  const [isPhotoLoaded, setIsPhotoLoaded] = useState(true)
+  const [isPhotoLoaded, setIsPhotoLoaded] = useState<boolean>(false)
 
   return (
     <VStack flex={1}>
@@ -17,8 +20,6 @@ export function Profile() {
       {/* ScrollView para habilitar uma rolagem da tela (motivo: ela vai ocupar toda e quando o usuário clicar no input vai subir o teclado) */}
       <ScrollView>
         <Center mt={6} px={10}>
-
-
           {
             isPhotoLoaded ?
               < Skeleton
@@ -37,9 +38,55 @@ export function Profile() {
                 size={PHOTO_SIZE}
                 mr={6}
               />
-
           }
+
+          <TouchableOpacity>
+            <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+
+
+          <Input
+            bg="gray.600"
+            placeholder="Nome"
+          />
+
+          <Input
+            bg="gray.400"
+            placeholder="E-mail"
+            isDisabled={true}
+          />
         </Center>
+
+        <VStack px={10} mt={12} mb={9}>
+          <Heading color="gray.200" fontSize="md" mb={2}>
+            Alterar senha
+          </Heading>
+
+          <Input
+            bg="gray.600"
+            placeholder="Senha antiga"
+            secureTextEntry={true}
+          />
+
+          <Input
+            bg="gray.600"
+            placeholder="Nova senha"
+            secureTextEntry={true}
+          />
+
+          <Input
+            bg="gray.600"
+            placeholder="Confirme a nova senha"
+            secureTextEntry={true}
+          />
+
+          <Button
+            mt={4}
+            title="Atualizar"
+          />
+        </VStack>
       </ScrollView>
 
     </VStack>
