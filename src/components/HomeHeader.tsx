f1b3361dff866a5d9bc5473d38f6e1058176315c
Renan <renan.fachin@hotmail.com>
2023-10-10 15:47:17 -0300

    feat: Cria método de remover dados do storage e função de signOut

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -7,7 +7,7 @@ import { useAuth } from "@hooks/useAuth";
 import defaultUserIMG from '@assets/userPhotoDefault.png'
 
 export function HomeHeader() {
-  const { user } = useAuth()
+  const { user, signOut } = useAuth()
 
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
@@ -29,7 +29,7 @@ export function HomeHeader() {
       </VStack>
 
 
-      <TouchableOpacity>
+      <TouchableOpacity onPress={signOut}>
         {/* https://docs.nativebase.io/icon#h3-integration-with-third-party-icons */}
         <Icon
           as={MaterialIcons}
--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,12 +2,13 @@ import { ReactNode, createContext, useEffect, useState } from "react";
 import { UserDTO } from "@dtos/UserDTO";
 import { api } from "@services/api";
 
-import { storageUserSave, storageUserGet } from "@storage/storageUser";
+import { storageUserSave, storageUserGet, storageUserRemove } from "@storage/storageUser";
 
 export type AuthContextDataProps = {
   user: UserDTO;
-  signIn: (email: string, password: string) => Promise<void> // promise pq é async
+  signIn: (email: string, password: string) => Promise<void>; // promise pq é async
   isLoadingUserStorageData: boolean;
+  signOut: () => Promise<void>;
 }
 
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps)
@@ -54,13 +55,30 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     }
   }
 
+  async function signOut() {
+    try {
+      setIsLoadingUserStorageData(true)
+
+      // Limpando o state
+      setUser({} as UserDTO)
+
+      // Limpando o storage
+      await storageUserRemove()
+
+    } catch (error) {
+      throw error
+    } finally {
+      setIsLoadingUserStorageData(false)
+    }
+  }
+
   useEffect(() => {
     loadUserDataFromStorage()
 
   }, [])
 
   return (
-    <AuthContext.Provider value={{ user: user, signIn, isLoadingUserStorageData }}>
+    <AuthContext.Provider value={{ user: user, signIn, isLoadingUserStorageData, signOut }}>
       {children}
     </AuthContext.Provider>
   )
